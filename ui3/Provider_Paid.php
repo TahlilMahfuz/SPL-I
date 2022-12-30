c2b12ea5a0ea57a96b161f26ea628777911f6735
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2022-12-30 14:23:28 +0600

    Add files via upload

---
+++ ui3/Provider_Paid.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+if(isset($_POST['returntomain-btn'])){
+    unset($_SESSION['token']);
+    unset($_SESSION['notarrived']);
+    header("location:Post Provider Login.php");
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ServiceLagbe</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="background-color: rgb(237,237,237);">
+        <div class="container"><a class="navbar-brand" href="./index.php">ServiceLagbe</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="./About.php">About</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="./Contact.php">Contact</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="./All%20Services.php">All Services</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
+                </ul>
+                <form class="form-inline mr-auto" target="_self">
+<!--                    <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="border-secondary form-control search-field" type="search" id="search-field" name="search"></div>-->
+<!--                    <div class="form-group"><a class="btn btn-light action-button" role="button" href="#">Search&nbsp;</a></div>-->
+                </form>
+            </div><span class="actions"> <a class="btn btn-light action-button" role="button" href="./Post%20Provider%20Login.php">Welcome <?php echo $_SESSION['username'] ?></a><a class="btn btn-light action-button" role="button" href="./Logout.php">Logout</a></span></div>
+    </nav>
+    <div class="login-clean">
+        <div class="container">
+            <h1 class="display-4"><?php echo "Total:".$_SESSION['providerpayment']. "<br>";?></h1>
+            <p class="lead">Thank you for your service.Please accept client's payment.</p>
+            <form action="./Provider_Paid.php" method="post">
+                <button type="submit" name="returntomain-btn" class="btn btn-success">PAID</button>
+            </form>
+        </div>
+    </div>
+    <div class="footer-basic" style="background-color: rgb(237,237,237);">
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="./Terms.php">Terms</a></li>
+                <li class="list-inline-item"><a href="./Privacy%20Policy.php">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">ServiceLagbe © 2022</p>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ ui3/Signup For Client.php
@@ -0,0 +1,125 @@
+<?php
+$showAlert = false;
+$showError = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    include 'partials/_dbconnect.php';
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $password = $_POST["password"];
+    $cpassword = $_POST["password-repeat"];
+    // $exists=false;
+
+    // Check whether this email exists
+    $existSql = "SELECT * FROM `users` WHERE email = '$email'";
+    $result = mysqli_query($conn, $existSql);
+    $numExistRows = mysqli_num_rows($result);
+    if($numExistRows > 0){
+        // $exists = true;
+        $showError = "An account already exists against this email";
+    }
+    else{
+        // $exists = false;
+        if(($password == $cpassword)){
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO `users` ( `username`,`email`,`phone`, `password`, `dt`) VALUES ('$username','$email','$phone', '$hash', current_timestamp())";
+            $result = mysqli_query($conn, $sql);
+            if ($result){
+                $showAlert = true;
+            }
+        }
+        else{
+            $showError = "Passwords do not match";
+        }
+    }
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ServiceLagbe</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+        <div class="container"><a class="navbar-brand" href="./index.php">ServiceLagbe</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="./About.php">About</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="./Contact.php">Contact</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="./All%20Services.php">All Services</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#"></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#"></a></li>
+                </ul>
+                <form class="form-inline mr-auto" target="_self">
+<!--                    <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="border-secondary form-control search-field" type="search" id="search-field" name="search"></div>-->
+<!--                    <div class="form-group"><a class="btn btn-light action-button" role="button" href="#">Search&nbsp;</a></div>-->
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="register-photo">
+        <div class="form-container">
+            <form method="post" style="height: 600;">
+                <h2 class="text-center"><strong>Create</strong> an account.</h2>
+                <?php
+                if($showAlert){
+                    echo '<h5 class="text-center">Your account  is now created and you can login.</h5>';
+                }
+                if($showError){
+                    echo '<h5 class="text-center">'.$showError.'</h5>';
+                }
+                ?>
+                <div class="form-group"><input class="form-control" type="text" name="username" placeholder="Username"></div>
+                <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                <div class="form-group"><input class="form-control" type="text" name="phone" placeholder="Phone"></div>
+                <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                <div class="form-group"><input class="form-control" type="password" name="password-repeat" placeholder="Password (Repeat)"></div>
+<!--                <div class="form-check"><label class="form-check-label"><input class="form-check-input" type="checkbox">I agree to the license terms.</label></div>-->
+                <button class="btn btn-primary btn-block" type="submit" style="background-color: rgb(102,215,215);">Sign Up</button>
+<!--                <a class="already" href="#">You already have an account? Login here.</a>-->
+            </form>
+        </div>
+    </div>
+    <div class="footer-basic" style="background-color: rgb(237,237,237);">
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="./Terms.php">Terms</a></li>
+                <li class="list-inline-item"><a href="./Privacy%20Policy.php">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">ServiceLagbe © 2022</p>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
