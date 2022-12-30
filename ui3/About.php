5d26f6ccc7ec2babdb9df12334dbe0cd27a48fd7
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2022-12-30 14:20:55 +0600

    Add files via upload

---
+++ ui3/About.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ServiceLagbe</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Actor">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Team-Clean.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+        <div class="container"><a class="navbar-brand" href="#">ServiceLagbe</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">About</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">All Services</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"></a></li>
+                </ul>
+                <form class="form-inline mr-auto" target="_self">
+                    <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" id="search-field" name="search"></div>
+                    <div class="form-group"><a class="btn btn-light action-button" role="button" href="#">Search&nbsp;</a></div>
+                </form>
+            </div><span class="actions"> <a class="btn btn-light action-button" role="button" href="">Log In</a><a class="btn btn-light action-button" role="button" href="#">Sign Up</a></span></div>
+    </nav>
+    <div class="team-clean"></div>
+    <div class="article-clean">
+        <section>
+            <h1 style="font-family: 'Open Sans', sans-serif;font-size: 35px;color: rgb(13,13,12);padding-right: 0px;padding-left: 685px;">About Us</h1>
+            <h1 style="font-family: 'Open Sans', sans-serif;font-size: 25px;color: rgb(13,13,12);padding-right: 0px;padding-left: 690px;padding-top: 10px;height: 48px;">Who We Are</h1>
+            <p style="padding-left: 15px;padding-top: 20px;height: 240px;font-size: 20px;padding-right: 15px;background-color: #dff8e1;padding-bottom: 2px;">ServiceLagbe is a fast-growing online home services platform in Bangladesh. Launched in 2022, ServiceLagbe today operates in and Dhaka, Gazipur and Chittagong. The platform helps customers book reliable &amp; high quality services - beauty
+                treatments, massages, haircuts, home cleaning, handymen, appliance repair, painting, pest control and more - delivered by trained professionals conveniently at home. ServiceLagbe's vision is to empower millions of professionals worldwide
+                to deliver services at home like never experienced before. The Company raised Series&nbsp;F funding of USD 255 million in December 2022. The Series F round was led by new investors - Prosus Ventures, Dragoneer and Wellington Management
+                with participation from existing investors - Vy Capital, Tiger Global and Steadview. The latest round includes a primary capital infusion of USD 188 million and a secondary sale of approximately USD 67 million by select angels and early
+                investors.<br></p>
+            <div class="card-group" style="padding-top: 30px;height: 260px;">
+                <div class="card">
+                    <div class="card-body" style="padding-top: 50px;padding-left: 130px;background-color: #6dc2ff;">
+                        <h4 class="card-title" style="color: rgb(11,11,11);font-family: 'Open Sans', sans-serif;font-size: 40px;">&nbsp; &nbsp; &nbsp; 10,000+</h4>
+                        <p class="card-text" style="font-size: 25px;">Trained service providers</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body" style="padding-top: 50px;padding-left: 130px;background-color: #6dc2ff;">
+                        <h4 class="card-title" style="color: rgb(11,11,11);font-family: 'Open Sans', sans-serif;font-size: 40px;">&nbsp; &nbsp; 200,000+</h4>
+                        <p class="card-text" style="font-size: 25px;">&nbsp; &nbsp; &nbsp; &nbsp;Customers<br></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body" style="padding-top: 50px;padding-left: 130px;background-color: #6dc2ff;">
+                        <h4 class="card-title" style="color: rgb(11,11,11);font-family: 'Open Sans', sans-serif;font-size: 40px;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 3</h4>
+                        <p class="card-text" style="font-size: 25px;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Cities</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container" style="padding-top: 30px;">
+                <div class="intro">
+                    <h2 class="text-center" style="color: #060606;font-size: 35px;">Our Team </h2>
+                </div>
+                <div class="row people">
+                    <div class="col-md-6 col-lg-4 item" style="background-color: #f7e8e8;"><img class="rounded-circle" src="assets/img/Tahlil%20(2).jpg">
+                        <h3 class="name" style="font-family: 'Open Sans', sans-serif;color: #060606;">&nbsp; &nbsp; &nbsp;Tahlil Mahfuz Faruk</h3>
+                        <p class="title" style="font-family: 'Open Sans', sans-serif;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; CEO &amp; Co-Founder, ServiceLagbe</p>
+                        <p class="description" style="font-family: 'Open Sans', sans-serif;color: rgb(10,10,10);background-color: #f7e8e8;">Tahlil leads technology and product development at ServiceLagbe. In his free time, Tahlil loves travelling, coding, games.</p>
+                        <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 item" style="height: 492.4px;background-color: #f7e8e8;"><img class="rounded-circle" src="assets/img/Muaz%20(2).jpg">
+                        <h3 class="name" style="font-family: 'Open Sans', sans-serif;color: #060606;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Muazul Islam</h3>
+                        <p class="title" style="font-family: 'Open Sans', sans-serif;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;CEO &amp; Co-Founder, ServiceLagbe<br></p>
+                        <p class="description" style="font-family: 'Open Sans', sans-serif;color: rgb(10,10,10);background-color: #f7e8e8;">Muaz is responsible for operations and service provider on-boarding at ServiceLagbe. Muaz likes to read books,spend time with family in his free time.</p>
+                        <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 item" style="background-color: #f7e8e8;"><img class="rounded-circle" src="assets/img/Shakkhor%20(2).jpg">
+                        <h3 class="name" style="font-family: 'Open Sans', sans-serif;color: #060606;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Tawfiq-E-Elahi</h3>
+                        <p class="title" style="font-family: 'Open Sans', sans-serif;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; CEO &amp; Co-Founder, ServiceLagbe<br></p>
+                        <p class="description" style="font-family: 'Open Sans', sans-serif;color: rgb(10,10,10);background-color: #f7e8e8;">Tawfiq is responsible for marketing and product growth at ServiceLagbe. When not busy,Tawfiq enjoys gaming,movies,sports,hangouts.</p>
+                        <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+            </div>
+            <h1 style="font-family: Oswald, sans-serif;font-size: 28px;color: rgb(13,13,12);padding-right: 0px;padding-left: 250px;height: 60px;padding-top: 20px;">Our mission is to deliver services at home in a way that has never been experienced in Bangladesh before</h1>
+            <p style="padding-left: 390px;padding-top: 0px;">For general queries, contact us: help@Servicelagbe.com|For media queries, contact us: press@Servicelagbe.com<br></p>
+            <h1 style="font-family: 'Open Sans', sans-serif;font-size: 20px;color: rgb(13,13,12);padding-right: 0px;padding-left: 600px;height: 40px;">Want to be a part of our journey?</h1>
+        </section>
+    </div>
+    <div class="footer-basic" style="background-color: rgb(237,237,237);">
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">ServiceLagbe © 2022</p>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
